84b6c6eb776de32e3ec5032b415c452f4e4e7154
clv3mppr <camping3307@gmail.com>
2025-10-20 15:35:13 +0300

    Add files via upload

---
+++ Module2Part3/Task1.py
@@ -0,0 +1,19 @@
+print("Введите число от 1 до 7: ")
+choice = int(input())
+
+if choice == 1:
+    print("Понедельник")
+elif choice == 2:
+    print("Вторник")
+elif choice == 3:
+    print("Среда")
+elif choice == 4:
+    print("Четверг")
+elif choice == 5:
+    print("Пятница")
+elif choice == 6:
+    print("Суббота")
+elif choice == 7:
+    print("Воскресенье")
+else:
+    print("Ошибка ввода операции")
---
+++ Module2Part3/Task2.py
@@ -0,0 +1,27 @@
+print("Введите число от 1 до 12: ")
+choice = int(input())
+
+if choice == 1:
+    print("Январь")
+elif choice == 2:
+    print("Февраль")
+elif choice == 3:
+    print("Март")
+elif choice == 4:
+    print("Апрель")
+elif choice == 5:
+    print("Май")
+elif choice == 6:
+    print("Июнь")
+elif choice == 7:
+    print("Июль")
+elif choice == 8:
+    print("Август")
+elif choice == 9:
+    print("Сентябрь")
+elif choice == 10:
+    print("Октябрь")
+elif choice == 11:
+    print("Ноябрь")
+elif choice == 12:
+    print("Декабрь")
