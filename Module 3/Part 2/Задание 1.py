6dd138b8d266b53f9984f1240803bca7de572d9b
clv3mppr <camping3307@gmail.com>
2025-10-20 14:16:15 +0300

    Add files via upload

---
+++ Module 3/Part 2/Задание 1.py
@@ -0,0 +1,27 @@
+from random import choice
+
+a = int(input())
+b = int(input())
+c = int(input())
+choice = str(input('Ведите sum для суммирования или multiply для суммирования: '))
+if choice == "sum":
+    print(a + b + c)
+elif choice == "multiply":
+    print(a * b * c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Module 3/Part 2/Задание 2.py
@@ -0,0 +1,12 @@
+a = int(5)
+b = int(6)
+c = int(7)
+number = int(input())
+if number == "max":
+else:
+
+
+
+
+
+
